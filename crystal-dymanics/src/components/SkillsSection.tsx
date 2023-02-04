798c90b7e5424ae389306c7682d1174636012436
Daffygzz <paulo_esilva@yahoo.com.br>
2023-02-04 00:57:55 -0300

    bug

--- crystal-dymanics/src/components/SkillsSection.tsx
+++ crystal-dymanics/src/components/SkillsSection.tsx
@@ -5,7 +5,7 @@ export function SkillsSection() {
     return (
         <section className="bg-violet-100 rounded m-5 py-3">
             <div className="container mx-auto">
-                <div className="grid  max-lg:grid-cols-5 max-md:grid-cols-6 max-sm:grid-cols-4">
+                <div className="grid max-xl:grid-cols-5  max-lg:grid-cols-5 max-md:grid-cols-6 max-sm:grid-cols-4">
                     {skills.map((skills, index) => {
                         return (
                             <div className="items-center justify-center backdrop-blur-[0.1px] border border-violet-500 m-2 rounded-lg text-cyan-50 hover:bg-violet-500
--- crystal-dymanics/src/screens/home.tsx
+++ crystal-dymanics/src/screens/home.tsx
@@ -1,7 +1,7 @@
 
 import { useTranslation } from "react-i18next"
 import { Hero } from "../components/Hero"
-import {SkillsSection} from "../components/SkillsSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export function Home() {
 
@@ -10,14 +10,15 @@ export function Home() {
     return (
         <div className="">
             <Hero />
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
-                {t('home.mySkills')}
-            </h1>
-            <p className="max-w-2xl mb-6 font-light text-200 lg:mb-8 md:text-lg lg:text-xl">
-                {t('home.textSkills')}
-            </p>
+            <div className="container mx-auto justify-start rounded m-5 ">
+                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
+                    {t('home.mySkills')}
+                </h1>
+                <p className="max-w-2xl mb-6 font-light text-200 lg:mb-8 md:text-lg lg:text-xl text-violet-200">
+                    {t('home.textSkills')}
+                </p>
+            </div>
             <SkillsSection />
-            <Hero />
         </div>
     )
 }
