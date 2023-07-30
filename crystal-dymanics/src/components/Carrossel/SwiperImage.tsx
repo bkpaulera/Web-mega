b9443823b218a4ef04fbec1b03057d991dcd6ba7
Paulo <paulo_esilva@yahoo.com.br>
2023-07-30 17:56:44 -0300

    carrosel

---
+++ crystal-dymanics/src/components/Carrossel/SwiperImage.tsx
@@ -0,0 +1,45 @@
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { skills } from '../../data/data';
+
+export function SwiperImage() {
+    const responsive = {
+        0: { items: 2 },
+        568: { items: 3 },
+        1024: { items: 4 },
+    };
+
+    const items = skills.map((skills, index) => {
+        return (
+            <div className="item">
+                <div key={index}>
+                    <div
+                        className='swiper-slide w-auto m-[10px] cursor-pointer p-5
+                hover:bg-zinc-300 rounded-sm'
+                    >
+
+                        <img
+                            src={skills.src}
+                            className='w-full h-auto rounded-lg shadow-2xl object-cover max-h-full'
+                            alt={`Skill ${index + 1}`}
+                        />
+                    </div>
+                </div>
+            </div>);
+    });
+
+    return (
+        <AliceCarousel
+            autoPlay
+            autoPlayStrategy="none"
+            autoPlayInterval={1500}
+            animationDuration={1500}
+            disableButtonsControls={true}
+            disableDotsControls={true}
+            infinite
+            mouseTracking
+            responsive={responsive}
+            items={items}
+        />
+    );
+}
